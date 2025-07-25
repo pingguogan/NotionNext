963896d2ae8ec597f9f250128bad3dfa3650526a
shendu <rc338464@gmail.com>
2025-07-26 00:13:43 +0800

    清空自定义js

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,16 +1,1 @@
-// 等待播放器加载并调整样式
-function adjustAPlayerPosition() {
-  const player = document.querySelector('.aplayer.aplayer-fixed');
-  if (player) {
-    player.style.bottom = '60px';
-    player.style.zIndex = '999'; // 确保播放器不被其他元素遮挡
-  } else {
-    // 如果找不到播放器，持续检查直到找到
-    setTimeout(adjustAPlayerPosition, 100); // 每 100ms 检查一次
-  }
-}
-
-// 页面加载后执行
-window.addEventListener('load', () => {
-  adjustAPlayerPosition();
-});
+// 这里编写自定义js脚本；将被静态引入到页面中
